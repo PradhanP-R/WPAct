d44060f3dcc698d278391fb1b4e540abe9fd161f
PradhanP-R <54267545+PradhanP-R@users.noreply.github.com>
2024-04-25 13:56:39 +0530

    Add files via upload

--- R/hello.R
+++ R/hello.R
@@ -1,9 +1,11 @@
 #' Package for searching names of plants and their synonyms mentioned in Wildlife (Protection) Act, 1972 (Amended as on date)'
-#' @param Test Excel file with one sheet and 1 column with heading 'Name' following by rows with species names
+#' @param Userdata Excel file with one sheet and 1 column with heading 'Name' following by rows with species names
 #' @author Prakash Pradhan, Purnendu Paul and Md. Maimun Habib
 #' @export
+#' @examples
+#' match_names("Userdata")
 #'
-match_names <- function(Userdata) {
+match_names <- function() {
   if(!require(readxl)){
     install.packages("readxl")
     library(readxl)
@@ -19,9 +21,10 @@ match_names <- function(Userdata) {
 
   # Load the data from the System File
   load("data/WPAdata.rda")
+  Userdata<-read_excel(file.choose())
   #Load user data. It has to be replaced with User Excel
-  Userdata <- data.frame(Name = c("Passiflora indica", "Cryptomeria japonica", "Araucaria imbricata", "Alnus nepalensis", "Wilcoxia nerispina"))
-  as_tibble(Userdata)
+  #Userdata <- data.frame(Name = c("Passiflora indica", "Cryptomeria japonica", "Araucaria imbricata", "Alnus nepalensis", "Wilcoxia nerispina"))
+  #as_tibble(Userdata)
   matches <- WPAdata %>%
   semi_join(Userdata, by = c("Scientific_Name" = "Name")) %>%
   select(Appendix, Scientific_Name)
@@ -31,4 +34,4 @@ match_names <- function(Userdata) {
 #match_names(Testdata)
 #Test<-read_excel(file.choose())
 #Userdata<-"C:/Users/91790/Downloads/WPAplants/R/Test.xlsx"
-match_names(Userdata)
+match_names()
