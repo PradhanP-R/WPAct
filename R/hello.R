a1a5b4d53892906cfbffec4e06ffe6ac6efa2af3
PradhanP-R <54267545+PradhanP-R@users.noreply.github.com>
2024-04-25 13:13:52 +0530

    Add files via upload

--- R/hello.R
+++ R/hello.R
@@ -3,7 +3,7 @@
 #' @author Prakash Pradhan, Purnendu Paul and Md. Maimun Habib
 #' @export
 #'
-match_names <- function(Test) {
+match_names <- function(Userdata) {
   if(!require(readxl)){
     install.packages("readxl")
     library(readxl)
@@ -17,30 +17,18 @@ match_names <- function(Test) {
     library(stringr)
   }
 
-  #library(readxl)
-  #library(dplyr)
-  #library(stringr)
   # Load the data from the System File
   load("data/WPAdata.rda")
-  #load("data/Testdata.rda")
-  #system.file('data', package = "WPAplsearch")
-  #system.file("Testdata", package = "WPAplsearch")
-  #files <- lapply(list.files(system.file('data', package = 'WPAplsearch'), full.names = TRUE), read_excel)
-  # Load the data from the User Excel File
-  #DataFrame <- read_excel("C:/Users/91790/Downloads/WPAplants/R/WPA_Plants.xlsx", sheet = 1)
-  # Load the data from the User Excel File
-  #Test <- read_excel("C:/Users/91790/Downloads/WPAplants/R/Test.xlsx", sheet = 1)
-  #Test<-read_excel(file.choose())
-  # Extract the names from the column 'Name' of Excel file Test
-  #names_Test <- Test$Name
-  # Check if the text in the 'Scientific_Name' column of Excel file df contains any of the names mentioned in 'Name' column of Excel file Test
-  # and display only those names along with the corresponding 'Appendix' values
+  #Load user data. It has to be replaced with User Excel
+  Userdata <- data.frame(Name = c("Passiflora indica", "Cryptomeria japonica", "Araucaria imbricata", "Alnus nepalensis", "Wilcoxia nerispina"))
+  as_tibble(Userdata)
   matches <- WPAdata %>%
-    semi_join(Test, by = c("Scientific_Name" = "Name")) %>%
-    select(Appendix, Scientific_Name)
+  semi_join(Userdata, by = c("Scientific_Name" = "Name")) %>%
+  select(Appendix, Scientific_Name)
   return(matches)
 }
-#Testdata <- read_excel("C:/Users/91790/Downloads/WPAplants/R/Test.xlsx", sheet = 1)
+#Userdata <- read_excel("C:/Users/91790/Downloads/WPAplants/R/Test.xlsx", sheet = 1)
 #match_names(Testdata)
 #Test<-read_excel(file.choose())
-match_names(Test)
+#Userdata<-"C:/Users/91790/Downloads/WPAplants/R/Test.xlsx"
+match_names(Userdata)
